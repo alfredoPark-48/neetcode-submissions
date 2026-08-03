2e32c70bdf68c4a29f23f2a4b465f200daa0039a
Alfredo Park <parkalfredojeonghyun@gmail.com>
2026-08-02 23:53:52 -0600

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const duplicates = new Map<string, string[]>();
+        for (const str of strs) {
+            const charArr = Array.from({ length: 26 }, () => 0);
+            for (const char of str) {
+                const key = char.charCodeAt(0) - 97;
+                charArr[key]++;
+            }
+            const strKey = charArr.join();
+            if (!duplicates.has(strKey)) {
+                duplicates.set(strKey, [str]);
+            } else {
+                const strs = duplicates.get(strKey);
+                duplicates.set(strKey, [...strs, str]);
+            }
+           
+        }
+        return Array.from(duplicates.values());
+    }
+}
